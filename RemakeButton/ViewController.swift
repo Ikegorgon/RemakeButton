b3962b434b5b791f3d27a477e9d8bd2acbaf9caa
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-10-10 12:03:09 -0600

    Added in Button Method to change background colors

--- RemakeButton/ViewController.swift
+++ RemakeButton/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Button1: UIButton!
+    @IBAction func Color (_ sender: UIButton) {
+        var int = 0
+        let rand :UIColor
+        let redValue : CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let greenValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let blueValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        rand = UIColor (red : redValue, green : greenValue, blue : blueValue, alpha : CGFloat(1.0))
+        int += 1
+        if (int % 2 == 1) {
+            Button1.backgroundColor = .green
+        } else {
+            Button1.backgroundColor = .purple
+        }
+        view.backgroundColor = rand
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
