90cb74deba31fd3319bae39680ab6e67ae6f29ce
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-10-12 13:20:08 -0600

    imported UIKit and created new class in file

---
+++ RemakeButton/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  RemakeButton
+//
+//  Created by Lamb, Isaac on 10/12/17.
+//  Copyright © 2017 Lamb, Isaac. All rights reserved.
+//
+
+import UIKit
+
+public class SecondViewController : UIViewController {
+    
+}
